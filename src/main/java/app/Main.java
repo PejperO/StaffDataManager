13f9df84d9f8646f88689d91ffda1a19eb447bea
Mikołaj Polecki <miki.polecki@gmail.com>
2025-10-21 19:32:01 +0200

    Add Person model and usage in Main
    Introduced the Person class and Type enum in the model package. Updated Main.java to create and display Person instances.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,16 @@
 package app;
 
+import model.Person;
+import static model.Type.INTERNAL;
+import static model.Type.EXTERNAL;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Application started.");
+        Person person = new Person("1", "John", "Doe", "123456789", " ", "90010112345", INTERNAL);
+        Person person2 = new Person("2", "Jane", "Smith", "987654321", " ", "85050554321", EXTERNAL);
 
+        System.out.println("Person created: " + person.getFirstName() + " " + person.getLastName() + ", Type: " + person.getType());
+        System.out.println("Person created: " + person2.getFirstName() + " " + person2.getLastName() + ", Type: " + person2.getType());
     }
 }
---
+++ src/main/java/model/Person.java
@@ -0,0 +1,69 @@
+package model;
+
+public class Person {
+    private String personId;
+    private String firstName;
+    private String lastName;
+    private String mobile;
+    private String email;
+    private String pesel;
+    private Type type;
+
+    public Person(String personId, String firstName, String lastName, String mobile, String email, String pesel, Type type) {
+        this.personId = personId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.mobile = mobile;
+        this.email = email;
+        this.pesel = pesel;
+        this.type = type;
+    }
+
+    // Getters
+    public String getPersonId() {
+        return personId;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getMobile() {
+        return mobile;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPesel() {
+        return pesel;
+    }
+    public Type getType() {
+        return type;
+    }
+
+    // Setters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s %s (%s) - %s", personId, firstName, lastName, type, email);
+    }
+}
